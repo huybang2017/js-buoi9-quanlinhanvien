e6b58560d08ff9be2adf9c303430da62b20db0fc
huybang2017 <ndhbang2017@gmail.com>
2023-02-06 23:39:47 +0700

    fixing index.html

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ function searchEmployee() {
 
     renderTable(newEmployeeList);
 }
-// Hàm xoá nhân viên theo id
+// Hàm xoá nhân viên theo account
 function deleteEmployee(employeeAccount) {
     employeeList = employeeList.filter((employee) => {
         return employee.account !== employeeAccount;
@@ -58,6 +58,51 @@ function fixEmployee(employeeAccount){
     getElement("#luongCB").value = selectedEmployee.salary;
     getElement("#chucvu").value = selectedEmployee.competence;
     getElement("#gioLam").value = selectedEmployee.timeWork;
+
+    getElement("#tknv").disabled = true;
+}
+// Hàm cập nhật thông tin nhân viên
+function updateEmployee(){
+    let account = getElement("#tknv").value;
+    let name = getElement("#name").value;
+    let email = getElement("#email").value;
+    let password = getElement("#password").value;
+    let dateOfWork = getElement("#datepicker").value;
+    let salary = +getElement("#luongCB").value;
+    let competence = getElement("#chucvu").value;
+    let timeWork = +getElement("#gioLam").value;
+
+    const employee = new Employee(
+        account,
+        name,
+        email,
+        password,
+        dateOfWork,
+        salary,
+        competence,
+        timeWork,
+    )
+
+    let index = employeeList.findIndex((employee) => {
+        return employee.account === account;
+    });
+    employeeList[index] = employee;
+
+    renderTable(employeeList);
+    resetForm();
+}
+// Hàm reset giá trị của các input
+function resetForm(){
+    getElement("#tknv").value = "";
+    getElement("#name").value = "";
+    getElement("#email").value = "";
+    getElement("#password").value = "";
+    getElement("#datepicker").value = "";
+    getElement("#luongCB").value = "";
+    getElement("#chucvu").value = "";
+    getElement("#gioLam").value = "";
+
+    getElement("#tknv").disabled = false;
 }
 // Hàm hiển thị nhân viên
 function renderTable(employeeList) {
